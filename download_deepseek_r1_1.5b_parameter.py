ad377cfebafa68630b0035918af34dd0e7b78d80
LohitakshaRohila <160320778+LohitakshaRohila@users.noreply.github.com>
2025-05-30 22:44:32 +0530

    Update download_deepseek_r1_1.5b_parameter.py

--- download_deepseek_r1_1.5b_parameter.py
+++ download_deepseek_r1_1.5b_parameter.py
@@ -1,5 +1,4 @@
 from transformers import AutoTokenizer, AutoModelForCausalLM
-# to download Deepseek R1 for better Resume Formatting
 model_name = "deepseek-ai/DeepSeek-R1-Distill-Qwen-1.5B"
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 model = AutoModelForCausalLM.from_pretrained(model_name, torch_dtype="auto")
